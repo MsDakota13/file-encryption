ab60c639cd46a823fe5f2da395d744f4d2a3286a
Robert-Jan Eduard Koot <heyokajan@msn.com>
2015-06-10 21:36:03 +0200

    Crypto start

---
+++ file-encrypter/src/main/java/com/hro/encrypter/CryptoException.java
@@ -0,0 +1,11 @@
+package com.hro.encrypter;
+
+public class CryptoException extends Exception {
+
+	public CryptoException() {
+	}
+
+	public CryptoException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+}
---
+++ file-encrypter/src/main/java/com/hro/main/FileEncrypter.java
@@ -0,0 +1,21 @@
+package com.hro.main;
+
+import java.io.File;
+import com.hro.encrypter.*;
+
+public class FileEncrypter {
+	public static void main(String[] args) {
+		String key = "Mary has one cat1";
+		File inputFile = new File("document.txt");
+		File encryptedFile = new File("document.encrypted");
+		File decryptedFile = new File("document.decrypted");
+		
+		try {
+			CryptoUtils.encrypt(key, inputFile, encryptedFile);
+			CryptoUtils.decrypt(key, encryptedFile, decryptedFile);
+		} catch (CryptoException ex) {
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+}
